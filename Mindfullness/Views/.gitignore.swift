f23e595a07fdd1cea4c5398ecbcfa08990f5a0c6
Aditya Sharma <97342837+Adi3030@users.noreply.github.com>
2025-03-24 17:28:11 +0530

    removed ignored files

---
+++ Mindfullness/Views/.gitignore.swift
@@ -0,0 +1,38 @@
+//
+//  .gitignore.swift
+//  Mindfullness
+//
+//  Created by aditya sharma on 24/03/25.
+//
+
+# macOS
+.DS_Store
+
+# Xcode
+DerivedData/
+xcuserdata/
+*.xcworkspace
+*.xcuserstate
+
+# Swift Package Manager
+.build/
+.swiftpm/
+
+# CocoaPods
+Pods/
+Podfile.lock
+
+# Carthage
+Carthage/Build/
+
+# Fastlane
+fastlane/report.xml
+fastlane/Preview.html
+fastlane/screenshots/
+fastlane/test_output/
+
+# Archives
+*.xcarchive
+
+# Environment Variables
+*.env
